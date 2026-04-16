6e29c8d9f1c9a34d5e9bc4b85a02394b09954cc1
Ramesh krishna R <rameshkrishnartg@gmail.com>
2026-04-16 13:19:45 +0530

    Update rabbitProducer.js

--- src/rabbitProducer.js
+++ src/rabbitProducer.js
@@ -3,6 +3,14 @@ const config = require("./config")
 
 let channel;
 
+const connectRabbit = async () => {
+    const conn = await ampq.connect(config.rabbit.url);
+    channel = await conn.createChannel();
+    await channel.assertQueue(config.rabbit.queue, { durable: true });
+
+    console.log('✅[RabbitMQ] Producer connected')
+}
+
 
 
 const publishToQueue = async (data) => {
